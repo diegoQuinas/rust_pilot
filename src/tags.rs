04351c9c1c1393d2ddab83bc7314ee09678af903
Diego Perez Giordán <diego.giordan@cencosud.com>
2025-01-05 19:37:50 -0300

    develop(feat): added tags

---
+++ src/tags.rs
@@ -0,0 +1,15 @@
+use colored::Colorize;
+
+pub fn error_tag() -> String {
+    "[ERR]".red().to_string()
+}
+
+pub fn log_tag() -> String {
+    "[LOG]".on_white().black().bold().to_string()
+}
+pub fn info_tag() -> String {
+    "[INFO]".blue().to_string()
+}
+pub fn warning_tag() -> String {
+    "[WARN]".yellow().to_string()
+}
